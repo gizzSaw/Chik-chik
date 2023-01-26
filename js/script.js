3a4a1e22335065b57c477af69a42ed44e3cb7ae3
gizzSaw <gizzsaw@gmail.com>
2023-01-26 23:34:28 +0300

    nextSlide

--- js/script.js
+++ js/script.js
@@ -8,7 +8,49 @@ const removePreload = (elem) => {
 
 const startSlider = () => { 
     const sliderItems = document.querySelectorAll('.slider__item')
-    let activeSlider = 1
+    const sliderList = document.querySelector('.slider__list')
+    const btnPrevSlide = document.querySelector('.slider__arrow_left')
+    const btnNextSlide = document.querySelector('.slider__arrow_right')
+    
+    let activeSlide = 1
+    let position = 0
+
+    const checkSlider = () => {
+        if (activeSlide + 2 === sliderItems.length) {
+            btnNextSlide.style.display = 'none'
+        } else {
+            btnNextSlide.style.display = ''
+        }
+
+        if (activeSlide === 1) {
+            btnPrevSlide.style.display = 'none'
+        } else {
+            btnPrevSlide.style.display = ''
+        }
+    }
+
+    const nextSlide = () => {
+        sliderItems[activeSlide].classList.remove('slider__item_active')
+        position = -sliderItems[0].clientWidth * activeSlide
+        sliderList.style.transform = `translateX(${position}px)`
+        activeSlide += 1
+        sliderItems[activeSlide].classList.add('slider__item_active')
+        checkSlider()
+    }
+
+    const prevSlide = () => {
+        sliderItems[activeSlide].classList.remove('slider__item_active')
+        position = -sliderItems[0].clientWidth * (activeSlide - 2)
+        sliderList.style.transform = `translateX(${position}px)`
+        activeSlide -= 1
+        sliderItems[activeSlide].classList.add('slider__item_active')
+        checkSlider()
+    }
+
+    btnPrevSlide.addEventListener('click', prevSlide)
+    btnNextSlide.addEventListener('click', nextSlide)
+
+
     console.log('sliderItems', sliderItems, 'activeSlider ', activeSlider)
 }
 
@@ -28,7 +70,3 @@ const initSlider = () => {
 }
 
 window.addEventListener('DOMContentLoaded', initSlider)
-
-// 30-30
-
-console.log('30')
