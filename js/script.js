1c47c18d1b2bb7e90560d3064e670ddb036f8fac
gizzSaw <gizzsaw@gmail.com>
2023-02-03 20:12:10 +0300

    api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+const API_URL = 'https://round-reflective-uncle.glitch.me/api'
+
+/*
+Доступные методы:
+GET /api - получить список услуг
+GET /api?service={n} - получить список барберов
+GET /api?spec={n} - получить список месяца работы барбера
+GET /api?spec={n}&month={n} - получить список дней работы барбера
+GET /api?spec={n}&month={n}&day={n} - получить список свободных часов барбера
+POST /api/order - оформить заказ
+*/
+
 const addPreload = (elem) => {
     elem.classList.add('preload')
 }
@@ -82,4 +94,59 @@ const initSlider = () => {
     })
 }
 
-window.addEventListener('DOMContentLoaded', initSlider)
+const renderPrice = (wrapper, data) => {
+    data.forEach(item => {
+        const priceItem = document.createElement('li')
+        priceItem.classList.add('price__item')
+
+        priceItem.innerHTML = `
+            <span>${item.name}</span>
+            <span class="price__item-count">${item.price}</span>
+        `
+        wrapper.append(priceItem)
+    })
+
+    
+
+    /* 
+    <li class="price__item">
+        <span>Стрижка ножницами</span>
+        <span class="price__item-count">2500 руб</span>
+    </li>
+    */
+}
+
+const renderService = (wrapper, data) => {
+    
+}
+
+const initService = () => {
+    const priceList = document.querySelector('.price__list')
+    const reserveFieldsetService = document.querySelector('.reserve__fieldset_service')
+    priceList.textContent = ''
+    addPreload(priceList)
+
+    reserveFieldsetService.innerHTML = `<legend class="reserve__legend">Услуга</legend>`
+    addPreload(reserveFieldsetService)
+
+    fetch(API_URL)
+        .then(response =>  response.json())
+        .then(data => {
+            renderPrice(priceList, data)
+            removePreload(priceList)
+            return data
+        })
+        .then(data => {
+            renderService(reserveFieldsetService, data)
+            removePreload(reserveFieldsetService)
+        })
+    
+    
+}
+
+const init = () => {
+    initSlider()
+    initService()
+}
+
+window.addEventListener('DOMContentLoaded', init)
